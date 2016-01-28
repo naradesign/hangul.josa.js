c1a03e01bcd4dba27ef87d779abecedeb1ca41fb
정찬명 <dece24@gmail.com>
2016-01-28 12:04:33 +0900

    js code dry.

--- hangul.josa.js
+++ hangul.josa.js
@@ -2,25 +2,18 @@
 ;(function($){
     "use strict";
     $.fn.josa = function(josaInput){ // "이/가", "을/를", "은/는", "와/과", "로/으로" 중 하나를 인자로 받아야 한다.
-
-        // 종성 유무에 따라 출력할 "주격", "목적격", "보격", "접속", "부사격" 조사를 배열에 담아 둔다.
-        var josaOutput = {
-            "josa0"       : ["이(가)", "을(를)", "은(는)", "와(과)", "로(으로)"], // 어떤 조사를 붙여야 할지 알 수 없는 경우 이 값 중 하나를 출력.
-            "josa1"    : ["이", "을", "은", "과", "으로"], // 종성 있는 경우 이 값 중 하나를 출력.
-            "josa2"   : ["가", "를", "는", "와", "로"] // 종성 없는 경우 이 값 중 하나를 출력.
-        };
-
         // 선택한 DOM을 순환한다.
-        $(this).each(function(){
+        this.each(function(){
             var $this = $(this), // 선택한 DOM 요소.
                 isInput = $this.is("input"), // 인풋 요소인가? value 값을 취할 것이다.
                 isSelect = $this.is("select"), // 셀렉트 요소인가? "option:selected" 요소의 value 값을 취할 것이다.
+                s = ":selected",
                 string; // 선택한 DOM 요소에서 가져올 문자열을 담는 변수. DOM 형태에 따라서 다른 값을 담게 된다.
 
             if( isInput ){ // 인풋 요소인 경우.
                 string = $this.val();
             } else if( isSelect ){ // 셀렉트 요소인 경우.
-                string = $this.find(":selected").val();
+                string = $this.find(s).val();
             } else { // 텍스트 요소인 경우.
                 string = $this.text();
             }
@@ -29,7 +22,7 @@
             var printJosa = function(){
                 var $next = $this.next(),
                     str = string.replace(/[^가-힣a-z\d]/gi, ""), // 한글+영문+숫자만 남기고 공백과 특수문자(일문, 중문 포함)를 모두 제거.
-                    hasFinal = ( /[가-힣]$/.test(str) && (str.substr(-1).charCodeAt(0) - 0xac00) % 28 > 0 ) || ( /[가-힣]\d*[013678]$/.test(str) || /[a-z]\d*[1789]$/i.test(str) ) || ( /([clmnp]|[blnt](e)|[co](k)|[aeiou](t)|(mb|ng|lert))$/i.test(str) ), // 한글, 숫자, 알파벳 발음에 종성이 있는가?
+                    hasFinal = ( /[가-힣]$/.test(str) && (str.substr(-1).charCodeAt(0) - 0xac00) % 28 > 0 ) || ( /[가-힣]\d*[013678]$/.test(str) || /[a-z]\d*[1789]$/i.test(str) ) || ( /([clmnp]|[blnt](e)|[co](k)|[aeiou](t)|mb|ng|lert)$/i.test(str) ), // 한글, 숫자, 알파벳 발음에 종성이 있는가?
                     josa; // 덧붙일 조사의 최종 값.
 
                 var setJosa = function(josaCase){ // josaCase는 josaOutput 객체의 값으로써 출력해야 할 조사 값이고 배열이다.
@@ -56,11 +49,11 @@
 
                 // 문자열의 특징에 따라서 어떤 조사를 붙여야 할지 분기 처리한다.
                 if( !/([가-힣]|\d|[a-z])$/i.test(str) ){ // 어떤 조사를 붙여야 할지 모르는 경우.
-                    setJosa(josaOutput.josa0); // ["이(가)", "을(를)", "은(는)", "와(과)", "로(으로)"] 를 인자로 넘긴다.
+                    setJosa(["이(가)", "을(를)", "은(는)", "와(과)", "로(으로)"]); // ["이(가)", "을(를)", "은(는)", "와(과)", "로(으로)"] 를 인자로 넘긴다.
                 } else if( hasFinal ){ // 발음에 종성 있는 경우.
-                    setJosa(josaOutput.josa1); // ["이", "을", "은", "과", "으로"] 를 인자로 넘긴다.
+                    setJosa(["이", "을", "은", "과", "으로"]); // ["이", "을", "은", "과", "으로"] 를 인자로 넘긴다.
                 } else if( !hasFinal ){ // 발음에 종성 없는 경우.
-                    setJosa(josaOutput.josa2); // ["가", "를", "는", "와", "로"] 를 인자로 넘긴다.
+                    setJosa(["가", "를", "는", "와", "로"]); // ["가", "를", "는", "와", "로"] 를 인자로 넘긴다.
                 }
 
                 // 결정된 josa 값을 DOM 요소 우측에 출력.
@@ -87,7 +80,7 @@
                     if( isInput ){ // 인풋 이면.
                         string = $this.val(); // value 값을 다시 string에 할당.
                     } else if( isSelect ) { // 셀렉트 이면.
-                        string = $this.find(":selected").val(); // "option:selected" value 값을 다시 string에 할당.
+                        string = $this.find(s).val(); // "option:selected" value 값을 다시 string에 할당.
                     }
                     printJosa(); // 조사 붙이기 함수 다시 실행.
                 });
